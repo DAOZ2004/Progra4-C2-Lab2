66a452e59e228376393d8480da0efc76932dfaa7
DAOZ2004 <orellanadaniel461@gmail.com>
2026-04-07 15:31:22 -0600

    Arreglando

--- Reg.php
+++ Reg.php
@@ -2,7 +2,7 @@
 session_start();
 include 'ConectDB.php';
 
-$username = $_POST['Psername'];
+$username = $_POST['Username'];
 $password = md5($_POST['Password']);
 
 $sql = "SELECT * FROM usuarios WHERE Username='$username' AND Password='$password'";
@@ -10,7 +10,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $_SESSION['user'] = $username;
-    header("Location: dashboard.php");
+    header("Location: Dashboard.php");
 } else {
     echo "Credenciales incorrectas";
 }
