36f8615ee54a7c9ea52d33f6043d099a5cf622bd
DAOZ2004 <orellanadaniel461@gmail.com>
2026-04-07 15:40:32 -0600

    porfa

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
       
     <form action="Reg.php" method="POST">
         <h2>Login</h2>
-        <input type="text" name="username" placeholder="Usuario" required>
-        <input type="password" name="password" placeholder="Contraseña" required>
+        <input type="text" name="Username" placeholder="Usuario" required>
+        <input type="password" name="Password" placeholder="Contraseña" required>
         <button type="submit">Ingresar</button>
     </form>
 
